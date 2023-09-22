640eddd62d16b996562fd9beb20d63c09c101608
Fay Shaw <fayette@gmail.com>
2023-09-22 09:11:13 -0400

    edited plot_points arguments

--- app.py
+++ app.py
@@ -20,17 +20,21 @@ st.header("A safety walkscore for pedestrians")
 st.text("Interactive map of Malden with car crash data")
 
 address = st.sidebar.text_input("Enter an address", "442 Main Street Malden MA 02148")
-
 data = map_plot.geocode(address).json()  
 
-if not data:
-    st.sidebar.caption('<p style="text-align:right; color:pink;"> \
-                         Please enter a valid address.</p>', unsafe_allow_html=True)
+#while not data:
+#    error_message = st.sidebar.caption('<p style="text-align:right; color:pink;"> \
+#                       Please enter a valid address.</p>', unsafe_allow_html=True)
+#    address = st.sidebar.text_input("Enter an address", "442 Main Street Malden MA 02148")
+#    data = map_plot.geocode(address).json()  
 
 
+# Erase error message
+#if data:
+#    error_message.text("")
+    
+    
 crash_df = map_plot.load_data()
-
-
-m = map_plot.plot_points(address, data, crash_df)
+m = map_plot.plot_points(data, crash_df)
 
 folium_static(m, width=725)
